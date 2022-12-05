1fa8ae7b43466228944068f30cc2a88ead6d326b
Mopcho <valeri_petkov01@abv.bg>
2022-12-05 22:26:13 +0200

    basic config

---
+++ webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		bundle: path.resolve(__dirname, '..', './src/index.tsx'),
+	},
+	output: {
+		path: path.resolve(__dirname, '..', './build'),
+		filename: 'bundle.js',
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, '..', './src/index.html'),
+		}),
+	],
+};
